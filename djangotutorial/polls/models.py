078aac0235cb6f1ad48e1e07f4684f6bf23d3c6e
ha5804 <zixpotf5803@gmail.com>
2025-07-07 10:33:17 +0900

    Question 클래스 만들어보기

--- djangotutorial/polls/models.py
+++ djangotutorial/polls/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class Question(models.Model):
+    question_text = models.CharField(max_length = 200)
+    pub_data = models.DateTimeField("date published")
+
+
 # Create your models here.
