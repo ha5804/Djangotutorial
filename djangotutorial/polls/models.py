521a3b5ef57f5c5cc61594d2d8d070a0b15ec83f
ha5804 <zixpotf5803@gmail.com>
2025-07-07 10:37:53 +0900

    모델 활성화를 위해 installed_apps에 해당 문장 작성 - 앱을 현재 프로젝트에 포함시키는 과정

--- djangotutorial/polls/models.py
+++ djangotutorial/polls/models.py
@@ -4,5 +4,8 @@ class Question(models.Model):
     question_text = models.CharField(max_length = 200)
     pub_data = models.DateTimeField("date published")
 
-
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+    choice_text = models.CharField(max_length = 200)
+    votes = models.IntegerField(default = 0)
 # Create your models here.
